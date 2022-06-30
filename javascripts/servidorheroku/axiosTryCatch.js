6f511475acb717a5175eacdd3ec4f72791129043
fdoquezada <fdoquezadapuno@gmail.com>
2022-06-30 11:53:17 -0400

    actualizacion de clase

--- javascripts/servidorheroku/axiosTryCatch.js
+++ javascripts/servidorheroku/axiosTryCatch.js
@@ -1,15 +1,20 @@
+const { Console } = require("console");
+
 var axios=require("axios").default;
 
-async function coneccionFlasa(){
+async function coneccionFalsa(){
     try{
-    let respuesta = await axios.get('https://conexionfalsa.com/password');
+    //let respuesta = await axios.get('https://conexionfalsa.com/password');
+    let respuesta = await axios.get('https://pokeapi.com/api/v2/pokemon/25');
     console.log(respuesta.status);
     console.log(respuesta.data);
     }catch(err){
         console.log(err.message);
         throw "no se ṕueod ejecutar tu consulta";
+    }finally{
+        console.log("fin");
     }
     
 }
 
-coneccionFlasa();
+coneccionFalsa();
---
+++ javascripts/servidorheroku/jimp.js
@@ -0,0 +1,1 @@
+var jimp=require("jimp");
