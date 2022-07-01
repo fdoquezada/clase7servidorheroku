5610beaf4f337580ef8a187fc4cd8a98f6c9bed1
fdoquezada <fdoquezadapuno@gmail.com>
2022-07-01 13:13:07 -0400

    actuliacion de clase

--- javascripts/servidorheroku/index.js
+++ javascripts/servidorheroku/index.js
@@ -1,10 +1,21 @@
 //server con express
 //1 importar modulo
-const { application } = require('express');
+
 const express = require('express');
 //2 crear o iniciar ña aplicacion express
 const app = express();
 //3craer variable con el puerto ;
 const puerto =8081;// essto de podria cargar desde el archivo -env
 //4 creacion de la funcion para un metodo 
-application.get('/', function)
+app.get('/', function(resquest,response){
+    res.send("hola con express");
+
+})
+//ejecutar el server
+app.listen(puerto,function(){
+    console.log("servidor iniciado en el  puerto: " + puerto);
+
+})
+app.get('/', function(resquest,response){
+    res.send("hola con express");
+})
--- javascripts/servidorheroku/jimp.js
+++ javascripts/servidorheroku/jimp.js
@@ -2,8 +2,28 @@
 https://i.pinimg.com/originals/81/3e/56/813e5649cbdcf60531effccae407397f.jpg
  */
 const express = require('express')
+const { nextTick } = require('process')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('prueba con express'))
+app.get('/usuario', (req, res) => res.send('se ha cosnultado un usuario'))
+
+app.post('/consultado', (req, res) => res.send('se ha cosnultado un post'))
+//agregar una funcion que tenga parametros en la ruta 
+app.get('/autor/:idAutor/libro/:idLibro', (req, res) => res.send("enviandodatos"))
+
+//funcionalidad con dos callback
+app.get('/comida', (req, res, next) => res.send("enviandodatos"))
+
+app.get('/comida', (req, res, next) => res.send("enviandodatos"))
+//archivos estaticos
+app.use(express.static('public'));
+app.get('/foto', (req, res, next) => res.send("<img src='img/descarga.jpeg' />"));
+
+//leer jason
+
+
+
 app.listen(port, () => console.log(`Server corriendo en  puerto  ${port}!`))
+
